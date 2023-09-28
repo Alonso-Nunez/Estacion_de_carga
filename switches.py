d3f32546cc2c0b0a21a0c2c99b942682ae715e5a
Alonso-Nunez <alonsonunezvaz@gmail.com>
2023-09-27 21:34:43 -0600

    Creacion del programa de control de switches

---
+++ switches.py
@@ -0,0 +1,72 @@
+import RPi.GPIO as GPIO
+import tkinter as tk
+
+
+
+def io_inversor(instruccion):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(31,GPIO.OUT)
+    if instruccion == 0:
+        GPIO.output(31,GPIO.LOW)
+    elif instruccion == 1:
+        GPIO.output(31,GPIO.HIGH)
+    else:
+        print("Instrucción no encontrada")
+    GPIO.cleanup()
+
+
+def io_bateria(instruccion):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(29,GPIO.OUT)
+    if instruccion == 0:
+        GPIO.output(29,GPIO.LOW)
+    elif instruccion == 1:
+        GPIO.output(29,GPIO.HIGH)
+    else:
+        print("Instrucción no encontrada")
+    GPIO.cleanup()
+
+
+def color_inversor():
+    global color_in
+    if color_in==1:
+        btn1.configure(bg="red")
+        io_inversor(0)
+        color_in = 0
+        
+    else:
+        btn1.configure(bg="green")
+        io_inversor(1)
+        color_in = 1
+        
+
+def color_bateria():
+    global color_ba
+    if color_ba==1:
+        btn2.configure(bg="red")
+        io_bateria(0)
+        color_ba = 0
+        
+    else:
+        btn2.configure(bg="green")
+        io_bateria(1)
+        color_ba = 1
+    
+    
+
+
+w = tk.Tk()
+w.title("Switches")
+#Frame
+fm = tk.Frame(w)
+fm.grid(row = 0, column = 0)
+color_in = 0
+color_ba = 0
+#Boton 1
+btn1 = tk.Button(fm, text = "Inversor", command=color_inversor, height= 2, width= 20,bg="red",fg="white")
+btn1.grid(row= 1, column= 0)
+#Boton2
+btn2 = tk.Button(fm, text = "Bateria", command=color_bateria, height= 2, width= 20,bg="red",fg="white")
+btn2.grid(row= 2, column= 0)
+
+w.mainloop()
