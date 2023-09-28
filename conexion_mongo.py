003a74a867f364ea8e01791488bb1df4f381f5f2
Alonso-Nunez <alonsonunezvaz@gmail.com>
2023-09-27 21:38:23 -0600

    Creación del programa del conexion con MongoDB

---
+++ conexion_mongo.py
@@ -0,0 +1,17 @@
+import pymongo
+
+MONGO_HOST = "localhost"
+MONGO_PUERTO = "27017"
+MONGO_TIMEOUT = 1000
+MONGO_URL = "mongodb://" + MONGO_HOST + ":" + MONGO_PUERTO + "/"
+ID_REGISTRO = ""
+
+MONGO_BD = "Prueba_cargador"
+MONGO_COLECCION = "Bateria"  # "Entradas""Salida"
+
+
+def conectar(base_datos, bd_coleccion):
+    cliente = pymongo.MongoClient(MONGO_URL, serverSelectionTimeoutMS=MONGO_TIMEOUT)
+    base = cliente[base_datos]
+    coleccion = base[bd_coleccion]
+    return cliente, coleccion
