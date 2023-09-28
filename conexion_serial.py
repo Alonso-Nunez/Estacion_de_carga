a6376a1b7f83885cc752eb74de390e2873fc0f4f
Alonso-Nunez <alonsonunezvaz@gmail.com>
2023-09-27 22:31:18 -0600

    Intento de programa para el serial

---
+++ conexion_serial.py
@@ -0,0 +1,12 @@
+import serial
+import os, time
+
+SERIAL_PORT = "/dev/ttyS0"
+ # Enable Serial Communication
+port = serial.Serial(SERIAL_PORT, baudrate=9600, timeout=1)
+port.write("Información serial"+"\n\r")#Envia información
+dato = port.read(10)#Lee 10 bytes
+#port.readline() Lee toda una linea
+print (dato)
+port.close()
+
