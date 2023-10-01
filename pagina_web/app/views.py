c0aa5c04a96b89ddb28ffb7bea8147ebe2bee5f7
yoss2729 <yolo_0955@hotmail.com>
2023-10-01 15:47:07 -0600

    configuracion para las vistas

--- pagina_web/app/views.py
+++ pagina_web/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+def inicio(request):
+    return render(request, 'home.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
 
 # Create your views here.
--- pagina_web/pagina_web/settings.py
+++ pagina_web/pagina_web/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from environ import Env
+
+env = Env() 
+env.read_env 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-je7*z7%sqbml@!ix3a)dq)_#2j#jh2f31d3ux1jh4^lin6!vg1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", True)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'pagina_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/ "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
