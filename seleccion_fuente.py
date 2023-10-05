a79a93e4245b604b4c7473ee2a8fa579834e07d1
Alonso-Nunez <alonsonunezvaz@gmail.com>
2023-10-04 23:47:49 -0600

    Creacion de 3 funciiones documentadas

--- seleccion_fuente.py
+++ seleccion_fuente.py
@@ -15,10 +15,10 @@ def peso_ponderado(fuente,valor):
 
     Args:
         fuente (int): fuente seleccionada a ponderar
-        1   Panel solar
-        2   Aerogenerador
-        3   Bateria
-        4   Suministro CFE
+        0   Panel solar
+        1   Aerogenerador
+        x   Bateria
+        2   Suministro CFE
         valor (float): valor obtenido de la medicion de los voltajes
 
     Returns:
@@ -84,8 +84,55 @@ def pasa_cfe(instruccion):
     else:
         print("Instruccion no encontrada")
 
-     
-   
+def acomodo_ponderado(ponderadoPanel, ponderadoAero, ponderadoBateria, ponderadoCFE):
+    """
+    Funcion que crea un arreglo con los pesos ponderados acomododados de mayor a menor
+
+    Args:
+        ponderadoPanel (float): Valor ponderado de la lectura del Panel Solar
+        ponderadoAero (float): Valor ponderado de la lectura del Aerogenerador
+        ponderadoBateria (float): Valor ponderado de la bateria
+        ponderadoCFE (float): Valor ponderado de CFE
+
+    Returns:
+        arreglor array: arreglo ordenado con los valores de mayor a menor
+    """
+    arreglo = [float(ponderadoPanel), float(ponderadoAero), float(ponderadoBateria), float(ponderadoCFE)]
+    arreglo.sort(reverse=True)
+    return arreglo
+
+def activar_fuente(arreglo,pP ,pA ,pC):
+    """
+    Funcion que activa el paso de corriente de la fuente seleccionada
+
+    Args:
+        arreglo (array): arreglo con los valores de los pesos ponderados ordenados
+        pP (float): Valor ponderado de la lectura del Panel Solar
+        pA (float): Valor ponderado de la lectura del Aerogenerador
+        pC (float): Valor ponderado de la lectura de CFE
+    """
+    if arreglo[0] == float(pP):
+        pasa_panel(1)
+    elif arreglo[0] == float(pA):
+        pasa_aero(1)
+    elif arreglo[0] == float(pC):
+        pasa_cfe(1)
+    else :
+        pasa_cfe(0)
+        pasa_aero(0)
+        pasa_panel(0)
+
+def apagar_fuentes():
+    """
+    Función que apaga todas las fuentes
+
+    Returns:
+        bool: Retorna falso para que las fuentes no se activen hasya nuevo aviso
+    """
+    pasa_cfe(0)
+    pasa_aero(0)
+    pasa_panel(0)
+    return False
 
 try:
     voltajePanel = input("Ingresa el voltaje del Panel Solar: ")
@@ -94,7 +141,7 @@ try:
     #Ponderación de los valores obtenidos
     ponderadoPanel = peso_ponderado(0, voltajePanel)
     ponderadoAero = peso_ponderado(1, voltajeAero)
-    ponderadoCfe =  peso_ponderado(3, voltajeCfe)
+    ponderadoCfe =  peso_ponderado(2, voltajeCfe)
     #Creacion de un arreglo que se acomoda
     arreglo=[ponderadoCfe,ponderadoAero,ponderadoPanel]
     arreglo.sort(reverse=True)
