2b2062830db41a02b3638906219875180e2b5738
yoss2729 <yolo_0955@hotmail.com>
2023-11-09 05:00:51 -0600

    estilos y vistas

--- pagina_web/app/views.py
+++ pagina_web/app/views.py
@@ -6,20 +6,36 @@ def inicio(request):
 
 def dashboard(request):
     client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["db_prueba"]
+    db = client["Prueba_cargador"]
     #Graphic Batery
-    collection = db["Bateria"]
+    collection = db["Mediciones"]
     # Consulta de datos desde MongoDB
-    data = collection.find_one({}, {"_id": 0})
+    data1 = collection.find_one({}, {"_id": 0, "voltaje Inversor": 0 ,"intensidad Entrada":0,"intensidad Inversor":0,"temperatura Bateria":0, "Carga Bateria":0})
+    data2 = collection.find_one({},{"_id": 0, "voltaje Panel Solar":0, "voltaje Aerogenerador":0, "voltaje Bateria":0, "voltaje CFE":0, "voltaje Inversor": 0 ,"intensidad Entrada":0,"intensidad Inversor":0})
+    
     #data_list = list(data)
+    #Grapich fuentes
     pie_data = []
 
-    for key, value in data.items():
+    for key, value in data1.items():
      pie_data.append({
         "label": key,
         "value": value
     })
-    data_json = json.dumps(pie_data)
-    return render(request, 'dashboard.html', {'data_json': data_json})
+    data1_json = json.dumps(pie_data)
+    
+    #Grapich Bateria
+    pie_data2 = []
+
+    for key, value in data2.items():
+     pie_data2.append({
+        "label": key,
+        "value": value
+    })
+    data2_json = json.dumps(pie_data2)
+    return render(request, 'dashboard.html', {'data1_json': data1_json, 'data2_json': data2_json})
+
+
+    
 
 
