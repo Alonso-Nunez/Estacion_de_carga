62331bb5e4ad0ae0e5338971b9fc651c4123953d
Alonso-Nunez <alonsonunezvaz@gmail.com>
2023-12-10 20:35:49 -0600

    Borrado de test

--- test/test_conexion_mongo.py
+++
@@ -1,5 +0,0 @@
-from conexion_mongo import *
-
-
-arreglo = [12,12,12,140,12,3,3,45]
-insertar_datos(arreglo)
--- test/test_estado_carga.py
+++
@@ -1,7 +0,0 @@
-from estado_carga import *
-import time
-
-while True:
-    auto = cargaAuto()
-    time.sleep(5)
-
